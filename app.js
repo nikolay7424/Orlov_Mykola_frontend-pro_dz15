9028e51b02fd7f6216339ada5a0d61f36fca3ccb
nikolay7424 <nikolay7424@gmail.com>
2023-06-29 15:39:50 +0300

    refactor 2

--- app.js
+++ app.js
@@ -3,24 +3,38 @@ function memo() {
   // keys arr for deleting
   const keys = [];
 
-  return function(callback, number) {
-    if(cache[number]) {
-      return cache[number];
-    } else if(keys.length === 10) {
-      // deleting first property
-      // keys[0] is a number, and a number is a key for object
-      delete cache[keys[0]];
-      keys.splice(0,1);
+  return function(callback, ...args) {
 
-      // adding number to cache and to keys arr
-      cache[number] = callback(number);
-      keys.push(number);
-      return callback(number);
+    const result = [];
+
+    args.forEach(number => {
+      if(cache[number]) {
+        result.push(cache[number]);
+      } else if(keys.length === 10) {
+        // deleting first property
+        // keys[0] is a number, and a number is a key for object
+        delete cache[keys[0]];
+        keys.splice(0,1);
+  
+        // adding number to cache and to keys arr
+        cache[number] = callback(number);
+        keys.push(number);
+        result.push(callback(number));
+      } else {
+        // adding number to cache and to keys arr
+        cache[number] = callback(number);
+        keys.push(number);
+        result.push(callback(number));
+      }
+    });
+
+    // if multiple args - print each
+    if(Array.isArray(args))  {
+      result.forEach(number => {
+        console.log(number)
+      });
     } else {
-      // adding number to cache and to keys arr
-      cache[number] = callback(number);
-      keys.push(number);
-      return callback(number);
+      console.log(result[0])
     }
   }
 }
@@ -31,15 +45,13 @@ const callback = (number) => {
 
 const call = memo();
 
-console.log(call(callback, '0670000001'));
-console.log(call(callback, '0670000002'));
-console.log(call(callback, '0670000002')); //returns from cache
-console.log(call(callback, '0670000003'));
-console.log(call(callback, '0670000004'));
-console.log(call(callback, '0670000005'));
-console.log(call(callback, '0670000006'));
-console.log(call(callback, '0670000007'));
-console.log(call(callback, '0670000008'));
-console.log(call(callback, '0670000009'));
-console.log(call(callback, '0670000010'));
-console.log(call(callback, '0670000011')); //deletes first number in cache and put this number as last in cache
+call(callback, '0670000001');
+call(callback, '0670000002');
+call(callback, '0670000002'); //returns from cache
+call(callback, '0670000003');
+call(callback, '0670000004', '0670000005', '0670000006');
+call(callback, '0670000007');
+call(callback, '0670000008');
+call(callback, '0670000009');
+call(callback, '0670000010');
+call(callback, '0670000011'); //deletes first number in cache and put this number as last in cache
