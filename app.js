3cc5583751bdf51100cc25bf46eb26c3dd7b5324
nikolay7424 <nikolay7424@gmail.com>
2023-06-29 15:21:37 +0300

    refactor 2

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-function outer(callback) {
+function memo() {
   const cache = {};
   // keys arr for deleting
   const keys = [];
-  return function inner(number) {
+
+  return function(callback, number) {
     if(cache[number]) {
       return cache[number];
     } else if(keys.length === 10) {
@@ -11,15 +12,15 @@ function outer(callback) {
       delete cache[keys[0]];
       keys.splice(0,1);
 
-
+      // adding number to cache and to keys arr
       cache[number] = callback(number);
       keys.push(number);
-      return cache[number];
+      return callback(number);
     } else {
       // adding number to cache and to keys arr
       cache[number] = callback(number);
       keys.push(number);
-      return cache[number];
+      return callback(number);
     }
   }
 }
@@ -28,17 +29,17 @@ const callback = (number) => {
   return `+38${number}`;
 }
 
-const call = outer(callback);
+const call = memo();
 
-console.log(call('0670000001'));
-console.log(call('0670000002'));
-console.log(call('0670000002')); //returns from cache
-console.log(call('0670000003'));
-console.log(call('0670000004'));
-console.log(call('0670000005'));
-console.log(call('0670000006'));
-console.log(call('0670000007'));
-console.log(call('0670000008'));
-console.log(call('0670000009'));
-console.log(call('0670000010'));
-console.log(call('0670000011')); //deletes first number in cache and put this number as last in cache
+console.log(call(callback, '0670000001'));
+console.log(call(callback, '0670000002'));
+console.log(call(callback, '0670000002')); //returns from cache
+console.log(call(callback, '0670000003'));
+console.log(call(callback, '0670000004'));
+console.log(call(callback, '0670000005'));
+console.log(call(callback, '0670000006'));
+console.log(call(callback, '0670000007'));
+console.log(call(callback, '0670000008'));
+console.log(call(callback, '0670000009'));
+console.log(call(callback, '0670000010'));
+console.log(call(callback, '0670000011')); //deletes first number in cache and put this number as last in cache
